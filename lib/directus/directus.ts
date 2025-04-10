ba9f22dc1982e6dd5a4dea0e0dece6f891cee0d4
wintercode <kenfackjordanjunior@gmail.com>
2025-04-10 15:17:27 +0100

    Refactor directus.ts: remove unused imports and request for improved clarity

--- lib/directus/directus.ts
+++ lib/directus/directus.ts
@@ -1,10 +1,7 @@
-import { createDirectus, readItem, readItems, rest } from "@directus/sdk";
-import Collections from "./schema";
+import { createDirectus, rest } from "@directus/sdk";
 
 const directus = createDirectus(
   process.env.NEXT_BACKOFFICE ?? "http://localhost:8057"
 ).with(rest());
-
-directus.request(readItems("Job"));
 
 export default directus;
