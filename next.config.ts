e7fd0718fd2097bf4914a900c1841b80e2f036e9
wintercode <kenfackjordanjunior@gmail.com>
2025-04-11 13:18:23 +0100

    Refactor Directus initialization and update environment variables in configuration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     NEXT_PUBLIC_BACKOFFICE: process.env.NEXT_PUBLIC_BACKOFFICE,
+    NEXT_IMAGE_BASE: process.env.NEXT_IMAGE_BASE,
   },
 };
 
