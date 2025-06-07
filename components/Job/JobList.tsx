134d4dee86db28a60ca19129e190d4feb9d7b703
wintercode <kenfackjordanjunior@gmail.com>
2025-06-07 13:06:45 +0100

    Enhance JobList component to display job details with active/expired status and improved layout

--- components/Job/JobList.tsx
+++ components/Job/JobList.tsx
@@ -69,7 +69,79 @@ const JobList = ({ Jobs, dicrionary }: JobOfferProps) => {
         </ul>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {filteredJobs.length > 0 &&
-            filteredJobs.map((job, index) => {
+            filteredJobs
+              .filter((job: any) => new Date(job.expire) > new Date())
+              .sort(
+                (a, b) =>
+                  new Date(b.expire).getTime() - new Date(a.expire).getTime()
+              )
+              .map((job, index) => {
+                console.log("expires on", job.expire);
+                return (
+                  <div key={index} className="shadow-md shadow-black">
+                    <div className="w-full flex flex-col gap-4 mb-4 relative">
+                      <img
+                        src={`${process.env.NEXT_IMAGE_BASE}assets/${job.illustration}`}
+                        alt="article"
+                        className="w-full h-[250px] object-cover bg-slate-300"
+                      />
+                      <button
+                        className={clsx(
+                          "rounded px-2 py-1 absolute top-2 right-2",
+                          "text-[var(--primary)]",
+                          `${
+                            !job.expire || new Date(job.expire) > now
+                              ? "bg-green-200"
+                              : "bg-red-200"
+                          }`
+                        )}
+                      >
+                        {/* Active expire */}
+
+                        {!job.expire || new Date(job.expire) > now
+                          ? "Active"
+                          : "Expired"}
+                      </button>
+                    </div>
+                    <div className="p-4">
+                      <h4 className="font-semibold hidden md:block">
+                        {job.title}
+                      </h4>
+
+                      {/* <h6 className="font-semibold md:hidden">{job.title}</h6> */}
+                      <ul className="flex gap-2 my-4 flex-wrap">
+                        {job.tags &&
+                          job.tags.map((filter: string, index: number) => {
+                            return (
+                              <li key={index}>
+                                <button
+                                  className={clsx(
+                                    "rounded-full px-2 py-1",
+                                    "bg-[var(--primary)] text-white"
+                                  )}
+                                >
+                                  {filter}
+                                </button>
+                              </li>
+                            );
+                            //
+                          })}
+                      </ul>
+                      <p>{job.summary}</p>
+                      <Link
+                        href={`jobs/${job.id}`}
+                        className="flex gap-2 items-center text-[var(--primary)] font-semibold"
+                      >
+                        <p>{dicrionary.apply}</p> <ArrowRight size={24} />
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })}
+
+          {filteredJobs
+            .filter((job: any) => new Date(job.expire) <= new Date())
+            .map((job, index) => {
               return (
                 <div key={index} className="shadow-md shadow-black">
                   <div className="w-full flex flex-col gap-4 mb-4 relative">
